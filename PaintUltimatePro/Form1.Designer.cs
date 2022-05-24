fd5a69d7b0d2a604548b9fca771a392bf54242b8
IhorKlymenko <ihorklymenkoc@gmail.com>
2022-05-24 09:19:13 +0300

    Added new features image viewing color and brush selection saving images other

--- PaintUltimatePro/Form1.Designer.cs
+++ PaintUltimatePro/Form1.Designer.cs
@@ -29,97 +29,281 @@ namespace PaintUltimatePro
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.buttonSaveFileAs = new System.Windows.Forms.Button();
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.buttonOpenFolder = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonColor = new System.Windows.Forms.Button();
             this.pictureBox = new System.Windows.Forms.PictureBox();
-            this.Brush = new System.Windows.Forms.Label();
-            this.Colour = new System.Windows.Forms.Label();
-            this.brushBox = new System.Windows.Forms.ComboBox();
-            this.colourBox = new System.Windows.Forms.ComboBox();
+            this.trackBarSize = new System.Windows.Forms.TrackBar();
+            this.labelSize = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.timerBackgroundChange = new System.Windows.Forms.Timer(this.components);
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.checkBoxDoNow = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelbrush = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(9, 10);
+            this.tabControl.Margin = new System.Windows.Forms.Padding(2);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(782, 431);
+            this.tabControl.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.labelbrush);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Controls.Add(this.checkBoxDoNow);
+            this.tabPage1.Controls.Add(this.buttonSaveFile);
+            this.tabPage1.Controls.Add(this.buttonSaveFileAs);
+            this.tabPage1.Controls.Add(this.buttonOpenFile);
+            this.tabPage1.Controls.Add(this.buttonOpenFolder);
+            this.tabPage1.Controls.Add(this.checkBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.buttonColor);
+            this.tabPage1.Controls.Add(this.pictureBox);
+            this.tabPage1.Controls.Add(this.trackBarSize);
+            this.tabPage1.Controls.Add(this.labelSize);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(774, 405);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Paint";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveFileAs
+            // 
+            this.buttonSaveFileAs.Location = new System.Drawing.Point(670, 303);
+            this.buttonSaveFileAs.Name = "buttonSaveFileAs";
+            this.buttonSaveFileAs.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveFileAs.TabIndex = 17;
+            this.buttonSaveFileAs.Text = "Save As";
+            this.buttonSaveFileAs.UseVisualStyleBackColor = true;
+            this.buttonSaveFileAs.Click += new System.EventHandler(this.buttonSaveFileAs_Click);
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(561, 274);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFile.TabIndex = 16;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // buttonOpenFolder
+            // 
+            this.buttonOpenFolder.Location = new System.Drawing.Point(561, 303);
+            this.buttonOpenFolder.Name = "buttonOpenFolder";
+            this.buttonOpenFolder.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenFolder.TabIndex = 15;
+            this.buttonOpenFolder.Text = "OpenFolder";
+            this.buttonOpenFolder.UseVisualStyleBackColor = true;
+            this.buttonOpenFolder.Click += new System.EventHandler(this.buttonOpenFolder_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(555, 232);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(109, 17);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Stop presentation";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(558, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Your text";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(555, 175);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 20);
+            this.textBox1.TabIndex = 12;
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(555, 90);
+            this.buttonColor.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(70, 27);
+            this.buttonColor.TabIndex = 11;
+            this.buttonColor.Text = "Color";
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
             // 
             // pictureBox
             // 
-            this.pictureBox.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox.Location = new System.Drawing.Point(16, 11);
             this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(527, 450);
-            this.pictureBox.TabIndex = 0;
+            this.pictureBox.Size = new System.Drawing.Size(500, 375);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 6;
             this.pictureBox.TabStop = false;
             this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
             this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
             this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp_22);
             // 
-            // Brush
+            // trackBarSize
             // 
-            this.Brush.AutoSize = true;
-            this.Brush.Location = new System.Drawing.Point(571, 34);
-            this.Brush.Name = "Brush";
-            this.Brush.Size = new System.Drawing.Size(34, 13);
-            this.Brush.TabIndex = 1;
-            this.Brush.Text = "Brush";
-            this.Brush.Click += new System.EventHandler(this.label1_Click);
+            this.trackBarSize.Location = new System.Drawing.Point(577, 41);
+            this.trackBarSize.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBarSize.Minimum = 1;
+            this.trackBarSize.Name = "trackBarSize";
+            this.trackBarSize.Size = new System.Drawing.Size(158, 45);
+            this.trackBarSize.TabIndex = 10;
+            this.trackBarSize.Value = 1;
+            this.trackBarSize.Scroll += new System.EventHandler(this.trackBarSize_Scroll);
             // 
-            // Colour
+            // labelSize
             // 
-            this.Colour.AutoSize = true;
-            this.Colour.Location = new System.Drawing.Point(571, 86);
-            this.Colour.Name = "Colour";
-            this.Colour.Size = new System.Drawing.Size(37, 13);
-            this.Colour.TabIndex = 2;
-            this.Colour.Text = "Colour";
-            this.Colour.Click += new System.EventHandler(this.label1_Click_1);
+            this.labelSize.AutoSize = true;
+            this.labelSize.Location = new System.Drawing.Point(635, 11);
+            this.labelSize.Name = "labelSize";
+            this.labelSize.Size = new System.Drawing.Size(27, 13);
+            this.labelSize.TabIndex = 7;
+            this.labelSize.Text = "Size";
             // 
-            // brushBox
+            // tabPage2
             // 
-            this.brushBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.brushBox.FormattingEnabled = true;
-            this.brushBox.Items.AddRange(new object[] {
-            "Small",
-            "Medium",
-            "Big"});
-            this.brushBox.Location = new System.Drawing.Point(574, 50);
-            this.brushBox.Name = "brushBox";
-            this.brushBox.Size = new System.Drawing.Size(121, 21);
-            this.brushBox.TabIndex = 3;
-            this.brushBox.SelectedIndexChanged += new System.EventHandler(this.brushBox_SelectedIndexChanged);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(774, 405);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // colourBox
+            // timerBackgroundChange
             // 
-            this.colourBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.colourBox.FormattingEnabled = true;
-            this.colourBox.Items.AddRange(new object[] {
-            "Black",
-            "Red",
-            "Blue"});
-            this.colourBox.Location = new System.Drawing.Point(574, 102);
-            this.colourBox.Name = "colourBox";
-            this.colourBox.Size = new System.Drawing.Size(121, 21);
-            this.colourBox.TabIndex = 4;
+            this.timerBackgroundChange.Enabled = true;
+            this.timerBackgroundChange.Interval = 1000;
+            this.timerBackgroundChange.Tick += new System.EventHandler(this.timerBackgroundChange_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Location = new System.Drawing.Point(670, 274);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveFile.TabIndex = 18;
+            this.buttonSaveFile.Text = "Save";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // checkBoxDoNow
+            // 
+            this.checkBoxDoNow.AutoSize = true;
+            this.checkBoxDoNow.Location = new System.Drawing.Point(670, 175);
+            this.checkBoxDoNow.Margin = new System.Windows.Forms.Padding(2);
+            this.checkBoxDoNow.Name = "checkBoxDoNow";
+            this.checkBoxDoNow.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxDoNow.TabIndex = 19;
+            this.checkBoxDoNow.Text = "DoNow";
+            this.checkBoxDoNow.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C"});
+            this.comboBox1.Location = new System.Drawing.Point(638, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 20;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // labelbrush
+            // 
+            this.labelbrush.AutoSize = true;
+            this.labelbrush.Location = new System.Drawing.Point(667, 78);
+            this.labelbrush.Name = "labelbrush";
+            this.labelbrush.Size = new System.Drawing.Size(34, 13);
+            this.labelbrush.TabIndex = 21;
+            this.labelbrush.Text = "Brush";
             // 
             // PaintUltimatePro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.colourBox);
-            this.Controls.Add(this.brushBox);
-            this.Controls.Add(this.Colour);
-            this.Controls.Add(this.Brush);
-            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.tabControl);
             this.Name = "PaintUltimatePro";
             this.Text = "PaintUltimatePro";
+            this.tabControl.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSize)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TrackBar trackBarSize;
+        private System.Windows.Forms.Label labelSize;
         private System.Windows.Forms.PictureBox pictureBox;
-        private System.Windows.Forms.Label Brush;
-        private System.Windows.Forms.Label Colour;
-        private System.Windows.Forms.ComboBox brushBox;
-        private System.Windows.Forms.ComboBox colourBox;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Timer timerBackgroundChange;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonOpenFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonSaveFileAs;
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.CheckBox checkBoxDoNow;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelbrush;
     }
 }
 
--- PaintUltimatePro/Form1.cs
+++ PaintUltimatePro/Form1.cs
@@ -1,73 +1,159 @@
 ﻿using System;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace PaintUltimatePro
 {
     public partial class PaintUltimatePro : Form
     {
+        private int _size;
+        private Color _color;
         private bool _moving;
+        private string[] _files;
+        private int _index;
 
         public PaintUltimatePro()
         {
             InitializeComponent();
-            brushBox.SelectedIndex = 0;
-            colourBox.SelectedIndex = 0;
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_1(object sender, EventArgs e)
-        {
-
-        }
-
-        private void brushBox_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
+            _size = 1; 
+            _index = 0;
+            _files = Directory.GetFiles(@"Images");
+            _color = Color.Black;
+            comboBox1.SelectedIndex = 0;
         }
 
         private void pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
-            _moving = true;
+            if (checkBoxDoNow.Checked)
+            {
+                var graphics = pictureBox.CreateGraphics();
+                using (Font myFont = new Font("Arial", 14))
+                graphics.DrawString(textBox1.Text, myFont, Brushes.Black, e.X, e.Y);
+
+            }
+            else
+            {
+                _moving = true;
+            }
+            
         }
 
         private void pictureBox_MouseMove(object sender, MouseEventArgs e)
         {
-            if (_moving==true)
+            if (_moving)
             {
-                var color = Brushes.Black;
-
-                if (colourBox.SelectedIndex == 0)
-                {
-                    color = Brushes.Black;
-                }
-                else if (colourBox.SelectedIndex == 1)
-                {
-                    color = Brushes.Red;
-                }
-                else
-                {
-                    color = Brushes.Blue;
-                }
-
-                int size = brushBox.SelectedIndex + 1;
                 var graphics = pictureBox.CreateGraphics();
-                graphics.FillRectangle(color, e.X, e.Y, size, size);
+                var brush = new SolidBrush(_color);
+                Pen pen = new Pen(brush);
+                switch (comboBox1.Text)
+                {
+                    case "A":
+                        graphics.FillEllipse(brush, e.X, e.Y, _size, _size);
+                        break;
+                    case "B":
+                        graphics.FillRectangle(brush, e.X, e.Y, _size, _size);
+                        break;
+                    case "C":
+                        graphics.DrawLine(pen, e.X, e.Y, _size, _size);
+                        break;
+                }
             }
-        }
-
-        private void pictureBox_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void pictureBox_MouseUp_22(object sender, MouseEventArgs e)
         {
             _moving = false;
+        }
+
+        private void trackBarSize_Scroll(object sender, EventArgs e)
+        {
+            _size = trackBarSize.Value;
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            if(colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                _color = colorDialog.Color;
+            }
+        }
+
+        private void timerBackgroundChange_Tick(object sender, EventArgs e)
+        {
+            
+            if (_files.Length > 0)
+            {
+                pictureBox.Image = Image.FromFile(_files[_index]);
+                _index = _index + 1 < _files.Length ? ++_index : 0;
+            }
+            else
+            {
+                timerBackgroundChange.Stop();
+            }
+        }
+
+        private void buttonOpenFolder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+                timerBackgroundChange.Start();
+            }
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox.Image = Image.FromFile(openFileDialog1.FileName);
+                checkBox1.Checked = true;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                timerBackgroundChange.Stop();
+            }
+            else
+            {
+                timerBackgroundChange.Start();
+            }
+        }
+
+        private void buttonSaveFileAs_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string save = Path.GetExtension(saveFileDialog1.FileName);
+                switch (save)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                pictureBox.Image.Save(saveFileDialog1.FileName, format);
+            }
+        }
+
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {
+            ImageFormat format = pictureBox.Image.RawFormat;
+            string name = pictureBox.ImageLocation;
+            pictureBox.Image.Save(name, format);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
